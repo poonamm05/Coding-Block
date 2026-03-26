71788fb450f3b4f3d0b58c7593af2e0a127e8801
poonamm05 <poonampatidar0755@gmail.com>
2026-03-26 10:45:55 +0530

    notes

---
+++ linear_search.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[i];
+    for(int i=0; i<n;i++){
+    cin>>a[i];
+
+int key;
+cin>>key;
+for(int i=0;i<n;i++){
+    if(key==a[i])
+    {
+        cout<<i<<endl;
+        return 0;
+    }
+}
+}
+cout<<-1<<endl;
+return 0;
+}
